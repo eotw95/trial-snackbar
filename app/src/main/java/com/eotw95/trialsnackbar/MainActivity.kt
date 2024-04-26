68edd14b4b390b93c62901b7887bf4be3e88c872
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-04-26 12:51:44 +0900

    Add basic pattern
    type = 1

--- app/src/main/java/com/eotw95/trialsnackbar/MainActivity.kt
+++ app/src/main/java/com/eotw95/trialsnackbar/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.eotw95.trialsnackbar.ui.theme.TrialSnackbarTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +34,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ShowSnackBar()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrialSnackbarTheme {
-        Greeting("Android")
+fun ShowSnackBar() {
+    val hostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = hostState)
+        }
+    ) {
+        Surface(modifier = Modifier.padding(it)) {
+            Button(
+                onClick = {
+                    coroutineScope.launch { hostState.showSnackbar("sample text") }
+                }
+            ) {
+                Text(text = "Show Snack bar")
+            }
+        }
     }
 }
